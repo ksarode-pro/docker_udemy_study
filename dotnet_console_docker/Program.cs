c38e33201b053fc6cea4c96a53db5b566b1c37fd
Kiran Sarode <ksarode.pro@gmail.com>
2025-01-29 18:39:26 +0530

    fist containerised dotnet core console app

---
+++ dotnet_console_docker/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num%2==0)
+System.Console.WriteLine($"{num} is even number.");
+else
+System.Console.WriteLine($"{num} is odd number.");
+ 
+ 
